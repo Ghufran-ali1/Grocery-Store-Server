0e01378ac5ef1cb48359aedc42cab4817f3df68c
Ghufran-ali1 <austineotieno577@gmail.com>
2025-09-19 20:08:14 +0300

    Initial commit

--- Origin.js
+++ Origin.js
@@ -1,7 +1,7 @@
 // allowedOrigins.js
 const allowedOrigins = [
-  'https://carshare-kenya.vercel.app',
-  'http://localhost:3000'
+  // 'https://carshare-kenya.vercel.app',
+  'http://localhost:5173'
 ];
 
 module.exports = allowedOrigins;
