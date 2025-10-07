4062dd728d23fe45b64d382a757e28e261eb9509
Ghufran-ali1 <austineotieno577@gmail.com>
2025-10-07 15:15:22 +0300

    Added views increment featuree

--- api/[main]/index.js
+++ api/[main]/index.js
@@ -143,14 +143,14 @@ if (main === 'update-item') {
   if (req.method !== 'PUT')
     return res.status(405).json({ status: 405, message: 'Method not allowed. Use PUT.' });
 
-  const { id, name, category, description, quantity, gallery } = req.body;
+  const { id, name, category, description, quantity, gallery, views } = req.body;
 
   console.log('received item: ', id, name, category, description, quantity, gallery)
 
   try {
     const result = await pool.query(
-      'UPDATE ghufran_store_items SET name=$1, description=$2, quantity=$3, category=$4, gallery=$5 WHERE id=$6 RETURNING *',
-      [name,  description, parseInt(quantity), category, JSON.stringify(gallery), id]
+      'UPDATE ghufran_store_items SET name=$1, description=$2, quantity=$3, category=$4, gallery=$5, views=$6 WHERE id=$7 RETURNING *',
+      [name,  description, parseInt(quantity), category, JSON.stringify(gallery), views, id]
     );
 
     if (result.rowCount === 0) {
